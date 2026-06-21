c50bab954bbe2c987789f6cf48fc7892d07c5e39
Divyansh15322 <idivyansh88@gmail.com>
2026-06-21 21:52:18 +0530

    change urls

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://verify-fbfi7t0dh-divyansh15322s-projects.vercel.app/';
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://verifyai-ctqd.onrender.com';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
